3d2ee99af0906da372611157705dc3b0ec46b8a6
r4coder <ritheeshk2003@gmail.com>
2026-08-01 15:54:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function toCase(text) {
-  // write your code here
+  // Handle empty, null, or undefined input
+  if (!text) {
+    return "-";
+  }
+
   return text.toLowerCase() + "-" + text.toUpperCase();
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
+// const text = prompt("Enter text:");
 alert(toCase(text));
